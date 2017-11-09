827bbe2a876242532d9d63faa3836a01731a2953
Felipe Kuhne <fkuhne@pucrs.br>
2017-11-09 01:53:13 -0200

    Update

--- helloWorld/src/main.c
+++ helloWorld/src/main.c
@@ -5,13 +5,13 @@
 void timerCallback(void *args)
 {
   printf("ping!\n");
-  mgos_gpio_toggle(2);
+ 
   (void)args;
 }
 
 enum mgos_app_init_result mgos_app_init(void)
 {
-  mgos_gpio_set_mode(2, MGOS_GPIO_MODE_OUTPUT);
   mgos_set_timer(1000, true, timerCallback, NULL);
+
   return MGOS_APP_INIT_SUCCESS;
 }
